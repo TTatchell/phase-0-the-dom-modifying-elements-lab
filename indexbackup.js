1b942abbd3bcc1772402fd4fd833e0d22b200783
TTatchell <tim.tatchell24@gmail.com>
2021-03-12 15:55:49 +1100

    upload

---
+++ indexbackup.js
@@ -0,0 +1,17 @@
+const element = document.createElement('div');
+
+document.body.appendChild(element);
+
+const ul = document.createElement('ul');
+
+for (let i = 0; i < 3; i++) {
+  const li = document.createElement('li');
+  li.innerHTML = (i + 1).toString();
+  ul.appendChild(li);
+}
+element.appendChild(ul);
+
+element.style.backgroundColor = '#27647B';
+element.style.fontSize = '24px';
+element.style.marginLeft = '30px';
+element.style.lineHeight = 2;
